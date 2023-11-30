32278ae8f7b4edf74c658400736f842243678b85
Mateus A. Carvalho <mateusabreu94@hotmail.com>
2023-11-30 05:34:30 -0300

    Style finished The style has done. Only thing left is entrance and exit animations.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,13 +2,26 @@ const bodyEl = document.querySelector('body');
 const blurBtnEl = document.querySelector('#blur-btn');
 const formEl = document.querySelector('#form')
 const sectionEl = document.querySelector('#main-section');
+const closeBtn = document.querySelector('#close-toogle');
 
 blurBtnEl.addEventListener("click", () => {
   bluringBgc(bodyEl, sectionEl, formEl);
+})
+
+document.addEventListener("click", (e) => {
+  let element = e.target;
+
+  if(element === bodyEl || element === closeBtn || element === sectionEl) return closingElement(formEl, bodyEl, sectionEl);
 })
 
 function bluringBgc(bgcBlurred, elementBlurred, elementToAppear) {
   bgcBlurred.classList.add('bgc-blurred', 'transition-time');
   elementBlurred.classList.add('blurred', 'transition-time');
   elementToAppear.style.visibility = 'visible';
+}
+
+function closingElement (element, bgcBlurred, elementBlurred) {
+  element.style.visibility = 'hidden';
+  bgcBlurred.classList.remove('bgc-blurred');
+  elementBlurred.classList.remove('blurred');
 }
